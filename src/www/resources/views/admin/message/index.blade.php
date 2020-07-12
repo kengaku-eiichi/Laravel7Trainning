af9d698d3d91a7f548c6552779f86109051fba84
kengaku <keng9358@gmail.com>
2020-07-12 14:34:23 +0900

    リファクタリング

--- src/www/resources/views/admin/message/index.blade.php
+++ src/www/resources/views/admin/message/index.blade.php
@@ -10,7 +10,7 @@
     </tr>
     @foreach($messages as $message)
     <tr>
-        <td><a href="{{ route('admin.message.edit', $message->id) }}">変更</a> </td>
+        <td><a href="{{ route('admin.message.edit', $message) }}">変更</a> </td>
         <td>{{ $message->user->name }}</td>
         <td>{{ $message->title }}</td>
         <td>{{ Str::limit($message->content, 50) }}</td>
